2a7505b8faeebc8c48dc5ca95460a981a7b04d97
antoniocaldeira@yahoo.com <antoniocaldeira@yahoo.com>
2021-01-05 12:17:09 +0000

    Criação de Controller

---
+++ RunnuingShoes/Controllers/HellNoWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RunnuingShoes.Controllers
+{
+    public class HellNoWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
