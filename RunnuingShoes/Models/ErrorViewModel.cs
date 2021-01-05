61698be2cebae495de5a0f548ceb9ab445e13591
antoniocaldeira@yahoo.com <antoniocaldeira@yahoo.com>
2021-01-05 11:46:37 +0000

    Add project files.

---
+++ RunnuingShoes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RunnuingShoes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
